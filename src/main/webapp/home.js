8c563ce0352faeba97ddcdf9b4836d8134df5ef6
Xavier <xluna600@gmail.com>
2020-06-26 21:26:26 +0000

    Add option to select book/movies/all,Add tabs to display movie list or book list,movie and book tabs display according to the selection the user makes. loadBooks() is commented out since book-search servlet isn't available

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -1,7 +1,28 @@
+function loadContent() {
+    document.getElementById("movie-tab").style.display = "none";
+    document.getElementById("book-tab").style.display = "none";
+    const option = document.getElementById('selection').value;
+
+    if(option === "movie") {
+        document.getElementById("movie-tab").style.display = "block";
+        loadMovies();
+    } else if(option === "book"){
+        document.getElementById("book-tab").style.display = "block"
+        loadBooks();
+    } else {
+        document.getElementById("movie-tab").style.display = "block"
+        loadMovies();
+
+        document.getElementById("book-tab").style.display = "block";
+        loadBooks();
+    }
+}
+
 function loadMovies() {
     const search = document.getElementById("search").value;
 
-    fetch('/movies/search?query=' + search + "&pageNumber=1").then(response => response.json()).then((data) => {
+    fetch('/movies/search?query=' + search + "&pageNumber=1")
+    .then(response => response.json()).then((data) => {
         const contentList = document.getElementById("movie-list");
         data.results.forEach((info) => {
             contentList.appendChild(createContentElement(info));
@@ -9,9 +30,29 @@ function loadMovies() {
     });
 }
 
+function loadBooks() {
+    console.log("book-search is unavailable at the moment");
+    /*const search = document.getElementById("search").value;
+
+    fetch('/books/search?query=' + search + "&pageNumber=1")
+    .then(response => response.json()).then((data) => {
+        const contentList = document.getElementById("book-list");
+        data.results.forEach((info) => {
+            contentList.appendChild(createContentElement(info));
+        })
+    });*/
+}
+
 //Creates an li to display the title of the movies
 function createContentElement(info) {
     const contentElement = document.createElement('li');
     contentElement.innerText = info.title;
     return contentElement;
+}
+
+function displayContent(content) {
+    document.getElementById("movie-list").style.display = "none";
+    document.getElementById("book-list").style.display = "none";
+
+    document.getElementById(content).style.display = "block";
 }
