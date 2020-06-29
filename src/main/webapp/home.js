d4953c75a24df1319cd1b845ea24f76655132f06
Xavier <xluna600@gmail.com>
2020-06-29 18:16:21 +0000

    Display list of books to book tab, clear list of element each time the user searches something new

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -1,15 +1,21 @@
 function loadContent() {
     document.getElementById("movie-tab").style.display = "none";
     document.getElementById("book-tab").style.display = "none";
+    document.getElementById("movie-list").innerHTML = "";
+    document.getElementById("book-list").innerHTML = "";
     const option = document.getElementById('selection').value;
 
+    //display's which tabs appear according to the selection of the user
     if(option === "movie") {
         document.getElementById("movie-tab").style.display = "block";
+        document.getElementById("book-list").style.display = "none";
         loadMovies();
     } else if(option === "book"){
         document.getElementById("book-tab").style.display = "block"
+        document.getElementById("movie-list").style.display = "none";
         loadBooks();
     } else {
+        document.getElementById("book-list").style.display = "none";
         document.getElementById("movie-tab").style.display = "block"
         loadMovies();
 
@@ -25,31 +31,37 @@ function loadMovies() {
     .then(response => response.json()).then((data) => {
         const contentList = document.getElementById("movie-list");
         data.results.forEach((info) => {
-            contentList.appendChild(createContentElement(info));
+            contentList.appendChild(createMovieElement(info));
         })
     });
 }
 
 function loadBooks() {
-    console.log("book-search is unavailable at the moment");
-    /*const search = document.getElementById("search").value;
+    const search = document.getElementById("search").value;
 
     fetch('/books/search?query=' + search + "&pageNumber=1")
     .then(response => response.json()).then((data) => {
         const contentList = document.getElementById("book-list");
         data.results.forEach((info) => {
-            contentList.appendChild(createContentElement(info));
+            contentList.appendChild(createBookElement(info));
         })
-    });*/
+    });
 }
 
-//Creates an li to display the title of the movies
-function createContentElement(info) {
-    const contentElement = document.createElement('li');
+//Creates a box to display the title of the movies
+function createMovieElement(info) {
+    const contentElement = document.createElement('div');
     contentElement.innerText = info.title;
     return contentElement;
 }
 
+function createBookElement(info) {
+    const contentElement = document.createElement('div');
+    contentElement.innerText = info.volumeInfo.title;
+    return contentElement;
+}
+
+//Display's wether the movies or books list appears
 function displayContent(content) {
     document.getElementById("movie-list").style.display = "none";
     document.getElementById("book-list").style.display = "none";
