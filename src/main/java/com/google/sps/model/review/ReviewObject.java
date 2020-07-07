ab001f11b1402bfb310d6962036814a84281239c
Henry Sloan <henryksloan@gmail.com>
2020-07-07 19:06:35 -0400

    Create constructor for semi-automatic population of ReviewObject fields

--- src/main/java/com/google/sps/model/review/ReviewObject.java
+++ src/main/java/com/google/sps/model/review/ReviewObject.java
@@ -1,11 +1,20 @@
 package com.google.sps.model.review;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.api.services.books.model.Volume;
+import com.google.sps.servlets.book.BookDetailsServlet;
+import com.google.sps.servlets.movie.MovieDetailsServlet;
+import com.google.sps.servlets.user.UserObject;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
+import info.movito.themoviedbapi.model.MovieDb;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.sql.Timestamp;
+
+import static com.google.sps.util.Utils.parseInt;
 
 @Entity
 public class ReviewObject {
@@ -13,6 +22,42 @@ public class ReviewObject {
     public ReviewObject() {
         Timestamp currentTime = new Timestamp(System.currentTimeMillis());
         this.timestamp = currentTime.getTime();
+    }
+
+    public ReviewObject(UserObject userObject,
+                        String contentType, String contentId,
+                        String reviewTitle, String reviewBody, int rating) throws IOException, GeneralSecurityException {
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        this.timestamp = currentTime.getTime();
+
+        this.authorId = userObject.getId();
+        this.authorName = userObject.getUsername();
+
+        this.contentType = contentType;
+        this.contentId = contentId;
+
+        switch (contentType) {
+            case "book":
+                Volume volume = new BookDetailsServlet().getDetails(contentId);
+                this.contentTitle = volume.getVolumeInfo().getTitle();
+                this.artUrl = volume.getVolumeInfo().getImageLinks().getThumbnail();
+                break;
+            case "movie":
+                Integer intId = parseInt(contentId);
+                if (intId == null) {
+                    throw new IllegalArgumentException();
+                }
+                MovieDb movie = new MovieDetailsServlet().getDetails(intId);
+                this.contentTitle = movie.getTitle();
+                this.artUrl = movie.getPosterPath();
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+
+        this.reviewTitle = reviewTitle;
+        this.reviewBody = reviewBody;
+        this.rating = rating;
     }
 
     @Id
@@ -26,7 +71,7 @@ public class ReviewObject {
 
     @JsonProperty
     @Index
-    private int rating;
+    private String authorId;
 
     @JsonProperty
     @Index
@@ -34,7 +79,11 @@ public class ReviewObject {
 
     @JsonProperty
     @Index
-    private String authorId;
+    private String contentType;
+
+    @JsonProperty
+    @Index
+    private String contentId;
 
     @JsonProperty
     @Index
@@ -46,11 +95,7 @@ public class ReviewObject {
 
     @JsonProperty
     @Index
-    private String contentType;
-
-    @JsonProperty
-    @Index
-    private String contentId;
+    private int rating;
 
     @JsonProperty
     @Index
