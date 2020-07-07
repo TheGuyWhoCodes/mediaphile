fef6f34c827c1007c1981631b2e05e9528034532
Henry Sloan <henryksloan@gmail.com>
2020-07-07 18:20:04 -0400

    Avoid using doGet functions to remove compile-time dependency on Mockito

--- src/main/java/com/google/sps/util/Utils.java
+++ src/main/java/com/google/sps/util/Utils.java
@@ -1,15 +1,9 @@
 package com.google.sps.util;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import com.google.sps.servlets.book.BookDetailsServlet;
 import com.google.sps.servlets.movie.MovieDetailsServlet;
-import org.mockito.Mockito;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
+import java.io.*;
 
 public class Utils {
     public static Integer parseInt(String s) {
@@ -22,33 +16,30 @@ public class Utils {
 
     // Leverages servlets to check if an item of type contentType exists with ID == contentId
     // Returns null if parameters are invalid
-    public static Boolean mediaItemExists(String contentType, String contentId) throws IOException {
+    public static Boolean mediaItemExists(String contentType, String contentId) {
         if (contentId.equals("")) {
             return null;
         }
 
-        HttpServletRequest request = Mockito.mock(HttpServletRequest.class);
-        Mockito.when(request.getParameter("id")).thenReturn(contentId);
-        HttpServletResponse response = Mockito.mock(HttpServletResponse.class);
-
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        Mockito.when(response.getWriter()).thenReturn(writer);
-
         try {
             switch (contentType) {
                 case "book":
-                    new BookDetailsServlet().doGet(request, response);
+                    new BookDetailsServlet().getDetails(contentId);
                     return true;
                 case "movie":
-                    new MovieDetailsServlet().doGet(request, response);
+                    Integer intId = parseInt(contentId);
+                    if (intId == null) return null;
+                    new MovieDetailsServlet().getDetails(intId);
                     return true;
                 default:
                     return null;
             }
         }
-        catch (Exception e) {
+        catch (IOException e) {
             return false;
+        }
+        catch (Exception e) {
+            return null;
         }
     }
 }
