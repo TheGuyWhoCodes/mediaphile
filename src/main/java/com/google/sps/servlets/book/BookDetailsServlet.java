09fad4324d7ea32607de3aaf52af4e239889f071
Henry Sloan <henryksloan@gmail.com>
2020-07-07 18:19:15 -0400

    Refactor details servlets to allow for easy serverside retrieval of details

--- src/main/java/com/google/sps/servlets/book/BookDetailsServlet.java
+++ src/main/java/com/google/sps/servlets/book/BookDetailsServlet.java
@@ -8,6 +8,7 @@ import com.google.api.services.books.Books;
 import com.google.api.services.books.model.Volume;
 import com.google.gson.Gson;
 import com.google.sps.KeyConfig;
+import org.mortbay.util.IO;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,10 +24,27 @@ public class BookDetailsServlet extends HttpServlet {
 
     private Gson gson = new Gson();
 
+    public Volume getDetails(String id) throws GeneralSecurityException, IOException {
+        final NetHttpTransport httpTransport;
+        try {
+            // Can throw an exception if trusted certificate cannot be established
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        }
+        catch (Exception e) {
+            throw new GeneralSecurityException();
+        }
+
+        Books books = new Books.Builder(httpTransport, jsonFactory, null)
+                .setApplicationName(KeyConfig.APPLICATION_NAME)
+                .build();
+
+        return books.volumes().get(id).execute();
+    }
+
     /**
      * doGet() returns details of the particular volume with the given id
      * Returns error 400 if no id is provided
-     * Returns error 500 if no book is returned by the API
+     * Returns error 404 if no book is returned by the API
      * @param request: expects id parameter
      * @param response: returns a Volume object
      * @throws IOException
@@ -41,22 +59,15 @@ public class BookDetailsServlet extends HttpServlet {
             return;
         }
 
-        final NetHttpTransport httpTransport;
         try {
-            // Can throw an exception if trusted certificate cannot be established
-            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            Volume volume = getDetails(id);
+            response.getWriter().println(gson.toJsonTree(volume));
         }
-        catch (Exception e) {
+        catch (GeneralSecurityException e) {
             response.sendError(HttpServletResponse.SC_BAD_REQUEST);
-            return;
         }
-
-        Books books = new Books.Builder(httpTransport, jsonFactory, null)
-                .setApplicationName(KeyConfig.APPLICATION_NAME)
-                .build();
-
-        Volume volume = books.volumes().get(id).execute();
-
-        response.getWriter().println(gson.toJsonTree(volume));
+        catch (IOException e) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
     }
 }
