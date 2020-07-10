470367df26a5b1e20ddaff0516d9a4adc4294f5d
Xavier <xluna600@gmail.com>
2020-07-10 12:40:36 +0000

    Display movie details on content page

--- src/main/webapp/contentDetails/content.js
+++ src/main/webapp/contentDetails/content.js
@@ -5,14 +5,32 @@ function goBack() {
 function loadDetails() {
     let id = localStorage.getItem('ident');
     let type = localStorage.getItem('type');
-    console.log(id);
-    console.log(type);
 
+    if(type === "movie") {
+        getMovieDetails(id);
+    } else {
+        getBookDetails(id);
+    }
 }
 
 function getBookDetails(id) {
     fetch('/books/details?id=' + id)
     .then(response => response.json()).then((info) => {
+        console.log(info);
+    });
+}
+
+function getMovieDetails(id) {
+    fetch('/movies/details?id=' + id)
+    .then(response => response.json()).then((info) => {
+        document.getElementById('title').innerText = info.title;
+        //document.getElementById('image').src = info.backdropPath;
+        document.getElementById('date').innerText = "release: " + info.releaseDate;
+        document.getElementById('runtime').innerText = info.runtime + "min";
+        document.getElementById('rating').innerText = "rating:" + info.voteAverage;
+        document.getElementById('orig-lang').innerText = info.originalLanguage;
+        document.getElementById('description').innerText = info.overview;
+
         console.log(info);
     });
 }
