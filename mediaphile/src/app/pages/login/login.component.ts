6dcc75382b44b97644f489c1012a20921aff2c45
Christopher Ariagno <christopherariagno@gmail.com>
2020-07-13 15:42:51 -0500

    Add login page

---
+++ mediaphile/src/app/pages/login/login.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {InfoService} from "../../info.service";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private infoSvc: InfoService) { }
+
+  ngOnInit(): void {
+  }
+
+  public login() {
+    this.infoSvc.login().subscribe(x => {
+      window.location.href = x["url"];
+    })
+  }
+}
