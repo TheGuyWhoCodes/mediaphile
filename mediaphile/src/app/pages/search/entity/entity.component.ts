fc4838b2f750b9ccf72fe9170a698f68adc0f26b
Christopher Ariagno <christopherariagno@gmail.com>
2020-07-13 14:29:05 -0500

    changes per henry's comments

--- mediaphile/src/app/pages/search/entity/entity.component.ts
+++ mediaphile/src/app/pages/search/entity/entity.component.ts
@@ -20,11 +20,11 @@ export class EntityComponent implements OnInit {
   }
 
   public getEntityImageUrl() : string {
-    if(this.type == "book") {
+    if(this.type === "book") {
       if("imageLinks" in this.entity["volumeInfo"]) {
         return this.entity["volumeInfo"]["imageLinks"]["thumbnail"]
       }
-    } else if(this.type == "movie") {
+    } else if(this.type === "movie") {
       if(this.entity["poster_path"]) {
         return "https://image.tmdb.org/t/p/w500" + this.entity['poster_path']
       }
@@ -33,11 +33,11 @@ export class EntityComponent implements OnInit {
   }
 
   public getReleaseYear() : string {
-    if(this.type == "movie") {
+    if(this.type === "movie") {
       if(this.entity["release_date"]) {
         return this.entity["release_date"].slice(0,4);
       }
-    } else if(this.type == "book") {
+    } else if(this.type === "book") {
       if(this.entity["volumeInfo"]["publishedDate"]) {
         return this.entity["volumeInfo"]["publishedDate"].slice(0,4);
       }
@@ -46,17 +46,17 @@ export class EntityComponent implements OnInit {
   }
 
   public getEntityUrl() : string {
-    if(this.type == "book") {
+    if(this.type === "book") {
       return `/book/${this.entity["id"]}`
-    } else if(this.type == "movie") {
+    } else if(this.type === "movie") {
       return `/movie/${this.entity["id"]}`
     }
   }
 
   public getEntityTitle() : string {
-    if(this.type == "movie") {
+    if(this.type === "movie") {
       return this.entity["title"];
-    } else if(this.type == "book") {
+    } else if(this.type === "book") {
       return this.entity["volumeInfo"]["title"];
     } else {
       return "N/A"
