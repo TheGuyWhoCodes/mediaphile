0a355a7735d57ad531822fe33a2451ad23e13a0d
Xavier <xluna600@gmail.com>
2020-07-14 20:33:02 +0000

    Add follow file to servlets folder

---
+++ src/main/java/com/google/sps/servlets/follow/FollowServlet.java
@@ -0,0 +1,23 @@
+package com.google.sps.servlets.follow;
+
+import com.google.gson.Gson;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/follow/list")
+public class FollowServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userId = request.getParameter("userId");
+        String list = request.getParameter("");
+
+        response.setContentType("application/json; charset=utf-8");
+        response.getWritter().println(gson.toJsonTree(result));
+    }
+}
