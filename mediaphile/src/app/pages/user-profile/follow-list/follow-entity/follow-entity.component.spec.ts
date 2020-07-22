61b2456858cc2dde98742308845137e574724ef3
Henry Sloan <henryksloan@gmail.com>
2020-07-22 10:25:39 -0400

    Fix misnamed test

--- mediaphile/src/app/pages/user-profile/follow-list/follow-entity/follow-entity.component.spec.ts
+++ mediaphile/src/app/pages/user-profile/follow-list/follow-entity/follow-entity.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FollowEntityComponent } from './follow-entity.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {InfoService} from "../../../../info.service";
+import {HttpClient, HttpHandler} from "@angular/common/http";
+import {LoginStatus} from "../../../../auth/login.status";
 
-describe('HomeEntityComponent', () => {
+describe('FollowEntityComponent', () => {
   let component: FollowEntityComponent;
   let fixture: ComponentFixture<FollowEntityComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FollowEntityComponent ]
+      declarations: [ FollowEntityComponent ],
+      imports: [HttpClientTestingModule, RouterTestingModule.withRoutes([])],
+      providers: [InfoService, HttpClient, HttpHandler, LoginStatus]
     })
     .compileComponents();
   }));
